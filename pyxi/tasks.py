5c9b49bcf9c685da9b028c51cb106bf9b5ad9749
Eric Scheier <eric@scheier.org>
2017-10-03 13:13:07 -0400

    Fix relative imports of pyxi methods
    Otherwise we get:
    `
    File "/home/sanyam/Work/freelance/altfund_website/venv/lib/python3.4/site-packages/pyxi/tasks.py", line 13, in <module>
       from pyxi import directRequest
    ImportError: cannot import name 'directRequest'
    `

--- pyxi/tasks.py
+++ pyxi/tasks.py
@@ -10,19 +10,19 @@ import configparser
 
 from Crypto.Cipher import AES
 from invoke import task
-from pyxi import directRequest
-from pyxi import requestAggregateOrderBooks
-from pyxi import requestBalance
-from pyxi import requestTradeHistory
-from pyxi import requestOpenOrders
-from pyxi import requestExchange
-from pyxi import request
-from pyxi import requestOrderBook
-from pyxi import requestLimitOrder
-from pyxi import cancelLimitOrder
-from pyxi import requestAvailableMarkets
-from pyxi import requestInterExchangeArbitrage
-from pyxi import requestFillOrKill
+from .pyxi import directRequest
+from .pyxi import requestAggregateOrderBooks
+from .pyxi import requestBalance
+from .pyxi import requestTradeHistory
+from .pyxi import requestOpenOrders
+from .pyxi import requestExchange
+from .pyxi import request
+from .pyxi import requestOrderBook
+from .pyxi import requestLimitOrder
+from .pyxi import cancelLimitOrder
+from .pyxi import requestAvailableMarkets
+from .pyxi import requestInterExchangeArbitrage
+from .pyxi import requestFillOrKill
 
 #balance, cancelorder, limitorder, openorders, orderbook, json, ticker, tradefees, tradehistory,
 
