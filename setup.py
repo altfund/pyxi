efaea4eba0e87e588e735cafd35ea6f2f2d4704b
gpwclark <gpwclark@gmail.com>
2018-02-11 23:39:04 +0000

    bump version number

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='pyxi',
-      version='0.52',
+      version='0.53',
       description='Python wrapper for xchange-interface API',
       url='http://github.com/altfund/pyxi',
       author='Altfund Capital',
